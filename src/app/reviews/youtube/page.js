9650f0ecd76584a8bd78c2382a226b7e3988ca24
Mohammed Riyas <its.mohd.riyas@gmail.com>
2024-04-09 01:10:33 +0530

    inital tailwind support

---
+++ src/app/reviews/youtube/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function YoutubeReview() {
+  return (
+    <div>YoutubeReview</div>
+  )
+}
+
+export default YoutubeReview
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,21 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'desktopui': '2560px',
+      // => @media (min-width: 2560px) { ... }
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
-  plugins: [],
+  plugins: [require("@designbycode/tailwindcss-text-shadow"),],
 };
