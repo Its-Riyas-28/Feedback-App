0ef0815b295a9d8c488a915402978db1fa64896c
Mohammed Riyas <its.mohd.riyas@gmail.com>
2024-04-24 03:29:21 +0530

    process:Developed sub pages

--- src/app/reviews/youtube/page.jsx
+++ src/app/reviews/youtube/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function YouTubeReview() {
   return (
-    <div className='bg-white'>
-        <h4>share your views</h4>
+    <div className='bg-white mt-7 rounded py-9 mx-8 pl-2 pt-4 '>
+        <h4 className='font-semibold'>Share Your Views</h4>
     </div>
   )
 }
