8fa2cefb92023e1d63feddc574e06566128a5f1f
Mohammed Riyas <its.mohd.riyas@gmail.com>
2024-04-27 00:14:40 +0530

    Refactor

--- src/components/Inputi.jsx
+++ src/components/Inputi.jsx
@@ -9,7 +9,7 @@ function Inputi({ children, placeholder, text }) {
       <input
         type="input"
         placeholder={placeholder}
-        className="bg-red-100 w-[330px] px-3 py-2 mt-1 rounded shadow-md shadow-red-400 outline-none"
+        className="bg-red-100 w-[290px] px-3 py-2 mt-1 rounded shadow-md shadow-red-400 outline-none"
       ></input>
       {children}
     </div>
